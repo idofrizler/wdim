dba4a4c89fb6f4abde9a0a042ef43fcfc6e3cc96
Ido Frizler <idof@microsoft.com>
2023-07-31 21:05:40 +0300

    Adding follow-up queries, 50 messages limit, resetting GPT context, saving to local storage

--- content.js
+++ content.js
@@ -1,9 +1,10 @@
+const OPENING_MESSAGE = "You are a helpful assistant that summarizes recent messages on thread in a few bullet points (with line breaks). There are potentially up to three parts to your response - a very concise summary of the messages starting with the prefix 'Summary:', a list of key dates mentioned starting with the prefix 'Key dates:', and a list of action items starting with the prefix 'Action items'.";
 const bodyJSON = {
     "model": "gpt-3.5-turbo",
     "messages": [
         {
             "role": "system",
-            "content": "You are a helpful assistant that summarizes recent messages on thread in a few bullet points (with line breaks). Start with general concise summary; I might ask for more details later."
+            "content": OPENING_MESSAGE
         }
     ]
 };
@@ -11,6 +12,10 @@ const bodyJSON = {
 chrome.runtime.onMessage.addListener(
     async function(request, sender) {
         switch(request.message) {
+            case "reset_gpt_context":
+                // cut the bodyJSON.message array and keep only the first element in it
+                bodyJSON.messages = bodyJSON.messages.slice(0, 1);
+                break;
             case "get_group_name":
                 const groupName = document.querySelector('span[data-testid="conversation-info-header-chat-title"]');
                 if (groupName) {
@@ -22,9 +27,12 @@ chrome.runtime.onMessage.addListener(
             case "get_messages":
                 const messagesDiv = document.querySelectorAll('div[role="row"]');
                 if (messagesDiv) {
+                    const maxMessages = 50;
+                    let startIndex = Math.max(0, messagesDiv.length - maxMessages);
                     let messageText = "";
                     let messageCount = 0;
-                    messagesDiv.forEach((message) => {
+                    for (let i = startIndex; i < messagesDiv.length; i++) {
+                        const message = messagesDiv[i];
                         const copyableText = message.querySelector('div.copyable-text');
                         if (copyableText) {
                             const span = copyableText.querySelector('span');
@@ -34,7 +42,7 @@ chrome.runtime.onMessage.addListener(
                                 messageCount++;
                             }
                         }
-                    });
+                    }
                     let messageSummary = "";
                     if (messageCount > 0) {
                         bodyJSON.messages.push({
@@ -43,15 +51,31 @@ chrome.runtime.onMessage.addListener(
                         });
                         messageSummary = await callOpenAI(bodyJSON);
                     }
-                    chrome.runtime.sendMessage({ type: "messages", dom: {messageCount: messageCount, messageSummary: messageSummary} });
+                    // split messageText by <br> and take first item
+                    const firstMessage = messageText.split("<br>")[0];
+                    // split firstMessage by ] and take first item, including the ]
+                    const firstMessagePrefix = firstMessage.split("]")[0] + "]";
+                    // Extract the date using Regex from the first message, which looks like this: [22:46, 21/07/2023]
+                    const dateRegex = /\[(\d{2}):(\d{2}), (\d{2})\/(\d{2})\/(\d{4})\]/;
+                    const match = firstMessagePrefix.match(dateRegex);
+                    const date = `${match[5]}-${match[4]}-${match[3]}T${match[1]}:${match[2]}:00`;
+                    // Calculate the time passed since that date, in local time
+                    const timePassed = new Date() - new Date(date);
+                    // Format the time passed in a string reading "x days and y hours ago"
+                    const daysPassed = Math.floor(timePassed / (1000 * 60 * 60 * 24));
+                    const hoursPassed = Math.floor((timePassed / (1000 * 60 * 60)) % 24);
+                    const timePassedString = `${daysPassed} days and ${hoursPassed} hours ago`;
+
+                    chrome.runtime.sendMessage({ type: "messages", dom: {messageCount: messageCount, timePassedString: timePassedString, messageSummary: messageSummary} });
                 } else {
                     chrome.runtime.sendMessage({ type: "messages", dom: "not found" });
                 }
                 break;
             case "get_details":
+                const followupQuery = request.followupQuery;
                 bodyJSON.messages.push({
                     "role": "user",
-                    "content": "Please be more descriptive. Keep writing in bullet points."
+                    "content": `${followupQuery}`
                 });
                 const messageSummary = await callOpenAI(bodyJSON);
                 chrome.runtime.sendMessage({ type: "messages", dom: { messageSummary: messageSummary } });
@@ -63,6 +87,8 @@ chrome.runtime.onMessage.addListener(
 );
 
 async function callOpenAI(bodyJSON) {
+    // return "Test data";
+
     const response = await fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
         headers: {
--- popup.js
+++ popup.js
@@ -4,23 +4,67 @@ document.addEventListener('DOMContentLoaded', function() {
             tabs[0].id,
             {message: "get_group_name"}
         );
-        chrome.tabs.sendMessage(
-            tabs[0].id,
-            {message: "get_messages"}
-        );
     });
 });
+
+function restoreMessagesFromStorage(currentGroupName) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.storage.local.get(['savedGroupName', 'messagesContent'], function(result) {
+            // If there is a saved group name and it's different from the current group name
+            if (result.savedGroupName && result.savedGroupName !== currentGroupName) {
+                // Clear the storage
+                chrome.storage.local.clear(function() {
+                    console.log('Storage cleared due to group name change');
+                    chrome.tabs.sendMessage(
+                        tabs[0].id,
+                        {message: "reset_gpt_context"}
+                    );
+                });
+            } else {
+                // Else, if there is saved messages content, load it into the #messages div
+                if (result.messagesContent) {
+                    document.getElementById('messages').innerHTML = result.messagesContent;
+                    document.getElementById('summary-paragraphs').style.display = 'block';
+                }
+            }
+        });
+    });
+}
+
+// save group name to storage
+function saveGroupName(groupName) {
+    chrome.storage.local.set({savedGroupName: groupName}, function() {
+        console.log('Group name saved: ' + groupName);
+    });
+}
 
 chrome.runtime.onMessage.addListener(
     function(request) {
         if (request.dom) {
-            if (request.type === 'group_name') {
-                document.getElementById('group-name').textContent = request.dom;
-            } else if (request.type === 'messages') {
-                if (request.dom.messageCount > 0) {
-                    document.getElementById('messageCount').textContent = request.dom.messageCount;
-                }
-                document.getElementById('messages').innerHTML = request.dom.messageSummary;
+            switch(request.type) {
+                case 'group_name':
+                    document.getElementById('group-name').textContent = request.dom;
+                    restoreMessagesFromStorage(request.dom);
+                    saveGroupName(request.dom);
+                    break;
+                case 'messages':
+                    if (request.dom.messageCount > 0) {
+                        document.getElementById('messageCount').textContent = request.dom.messageCount;
+                        // make summarize-text-date visible
+                        document.getElementById('summarize-text-date').style.display = 'block';
+                        document.getElementById('timePassedString').textContent = request.dom.timePassedString;
+                    }
+                    document.getElementById('messages').innerHTML = request.dom.messageSummary;
+
+                    chrome.storage.local.set({messagesContent: request.dom.messageSummary}, function() {
+                        console.log('Messages content saved');
+                    });
+
+                    // make details-input visible
+                    document.getElementById('details-input').style.display = 'block';
+                    break;
+                default:
+                    console.error("Invalid request type");
             }
         } else {
             console.error("Failed to get DOM from content script");
@@ -28,13 +72,33 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
-document.getElementById('details-button').addEventListener('click', function() {
+// when summarize-button is clicked, send a message to content.js to get the messages
+document.getElementById('summarize-button').addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(
             tabs[0].id,
-            {message: "get_details"}
+            {message: "reset_gpt_context"}
+        );
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            {message: "get_messages"}
         );
     });
-    document.getElementById('details-button').style.display = 'none';
-    document.getElementById('messages').innerHTML = 'Creating a more detailed summary...';
+    document.getElementById('summarize-button').style.display = 'none';
+    // show the summary paragraphs
+    document.getElementById('summary-paragraphs').style.display = 'block';
+});
+
+// when details-input is changed and Enter is pressed, send a message to content.js to get the details
+document.getElementById('details-input').addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                {message: "get_details", followupQuery: document.getElementById('details-input').value}
+            );
+        });
+        document.getElementById('details-input').style.display = 'none';
+    }
 });
